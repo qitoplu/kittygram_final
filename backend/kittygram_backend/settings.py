f39429668369d282b8caced6cdebd17fcb00f75d
qitoplu <213xixixi@gmail.com>
2023-07-14 13:58:59 +0300

    review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('HOSTS', '').split()
